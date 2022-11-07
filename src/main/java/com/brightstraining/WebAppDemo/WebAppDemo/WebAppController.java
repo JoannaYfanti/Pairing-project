23fb8a49a228edc09d4fb0a09e16f28253b7ac8a
Ioanna Yfanti <Ioanna.Yfanti@awacademy.traning>
2022-11-07 11:51:40 +0200

    add some comments to src

--- src/main/java/com/brightstraining/WebAppDemo/WebAppDemo/WebAppController.java
+++ src/main/java/com/brightstraining/WebAppDemo/WebAppDemo/WebAppController.java
@@ -4,6 +4,7 @@ package com.brightstraining.WebAppDemo.WebAppDemo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,21 +12,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class WebAppController {
 
     @GetMapping("/hey")
-    public ResponseEntity<String> hey(){
+    public ResponseEntity<String> hey() {
         return ResponseEntity.ok("Yeah you did it");
 
     }
 
     @GetMapping("/hello")
-    public ResponseEntity<String> hello(){
+    public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Yeah you did it again");
 
     }
 
     @GetMapping("/pair")
     @ResponseBody
-    public ResponseEntity<String> pair(@RequestParam String name, String food){
+    public ResponseEntity<String> pair(@RequestParam String name, String food) {
         return ResponseEntity.ok("Yeah you did it again\n" + name + "I like " + food);
 
     }
+
+//    @PostMapping("/helloo")
+//    @ResponseBody
+//    public String helloo(@RequestParam String name, String food) {
+//        return "Hello, " + name + "I like " + food;
+//    }
 }
