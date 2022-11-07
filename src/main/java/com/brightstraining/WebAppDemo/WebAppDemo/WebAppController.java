31d84bbe88ebfc79fc7b36c8bc50aa654fe1e394
Filippos Mantzios <mantzios_ph@hotmail.com>
2022-11-07 11:27:53 +0200

    added pair tab with 2 request parameters

--- src/main/java/com/brightstraining/WebAppDemo/WebAppDemo/WebAppController.java
+++ src/main/java/com/brightstraining/WebAppDemo/WebAppDemo/WebAppController.java
@@ -4,6 +4,8 @@ package com.brightstraining.WebAppDemo.WebAppDemo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebAppController {
@@ -20,4 +22,10 @@ public class WebAppController {
 
     }
 
+    @GetMapping("/pair")
+    @ResponseBody
+    public ResponseEntity<String> pair(@RequestParam String name, String food){
+        return ResponseEntity.ok("Yeah you did it again\n" + name + "I like " + food);
+
+    }
 }
